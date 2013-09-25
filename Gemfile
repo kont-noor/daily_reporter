c5c1d935452d8aa62f0812f136143debb75c7187
Nickolay Kondratenko <devmarkup@gmail.com>
2013-09-25 15:22:03 +0300

    report by sendmail

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "rake",  "~> 10.0.3"
+gem "mail",  "~> 2.5.4"
 gem "pry"
--- report.rb
+++ report.rb
@@ -1,21 +1,20 @@
 require 'ostruct'
 require 'net/imap'
+require 'mail'
 require 'yaml'
 require 'pry'
 
 module Reporter
   class << self
     def report
-      if email = get_mail_address
-        puts email
-      end
+      send_status(get_mail_address, get_status)
     end
+
+    private
 
     def settings
       @settings ||= get_settings
     end
-
-    private
 
     def get_settings
       settings_hash = YAML.load_file('settings.yml')
@@ -47,6 +46,30 @@ module Reporter
       envelope = imap.fetch(id, 'ENVELOPE').first.attr['ENVELOPE']
       reply_to = envelope.reply_to.first
       "#{reply_to.mailbox}@#{reply_to.host}"
+    end
+
+    def get_status
+      File.read(settings.report_file).strip
+    end
+
+    def send_status(email, status)
+      if email && status
+        username = settings.mail.username
+        mail = Mail.new do
+          from    username
+          to      email
+          subject 'Re: What have you done today?'
+          body    status
+        end
+
+        mail.delivery_method :sendmail
+        mail.deliver!
+        clear_status
+      end
+    end
+
+    def clear_status
+      File.open(settings.report_file, 'w') {}
     end
   end
 end
