71f173d614c615040d2332673d34e5d3ed4333bf
Nickolay Kondratenko <devmarkup@gmail.com>
2013-10-11 00:38:36 +0300

    fix tasks file absence

--- lib/daily-reporter/task.rb
+++ lib/daily-reporter/task.rb
@@ -3,18 +3,18 @@ module DailyReporter
     TASKS_FILE = DailyReporter::SETTINGS_DIRECTORY + '/tasks.txt'
     class << self
       def status
-        content = File.open(TASKS_FILE, 'r+') do |f|
+        content = File.open(TASKS_FILE, 'a+') do |f|
           f.read.strip
         end
         content.empty? ? nil : content
       end
 
       def clear_status
-        File.open(Settings.report_file, 'w') {}
+        File.open(TASKS_FILE, 'w') {}
       end
 
       def add(task)
-        File.open(Settings.report_file, 'a') do |f|
+        File.open(TASKS_FILE, 'a') do |f|
           f.puts task
         end
       end
