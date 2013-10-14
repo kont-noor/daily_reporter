f68c02eca174803dccf93769a8daf530f0637320
Nickolay Kondratenko <devmarkup@gmail.com>
2013-10-14 10:20:08 +0300

    cover settings dynamic proxy

--- spec/lib/daily_reporter/settings_spec.rb
+++ spec/lib/daily_reporter/settings_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe DailyReporter::Settings do
   include FakeFS::SpecHelpers
+
+  describe 'method missing' do
+    it "should redirect unknown method to settings" do
+      DailyReporter::Settings.class_eval do
+        settings.should_receive(:test_method).exactly(1).times
+      end
+
+      DailyReporter::Settings.test_method
+    end
+
+    it "should redirect private method to settings" do
+      DailyReporter::Settings.class_eval do
+        settings.should_receive(:settings).exactly(1).times
+      end
+
+      DailyReporter::Settings.settings
+    end
+
+    it "should not redirect public method to settings" do
+      DailyReporter::Settings.class_eval do
+        settings.should_receive(:init).exactly(0).times
+      end
+      DailyReporter::Settings.stub(:init)
+
+      DailyReporter::Settings.init
+    end
+  end
 end
